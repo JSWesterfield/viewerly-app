590c6b1def89b386fb5b5055ab7cd3027c3a2704
Jake Westerfield <jecobwesterfield@gmail.com>
2022-10-20 10:31:08 -0400

    adds in libraries and httpLink method to connect environmental variables with GraphQL

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+import 'webrtc-adapter';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-client-preset';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from 'react-apollo';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/index.scss';
+import routes from './routes';
+import store from './store';
+import { setToken } from './actions/token';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+store.dispatch(setToken(window.__JWT_TOKEN__));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const httpLink = new HttpLink({
+  uri: process.env.GRAPHQL_URI,
+  credentials: 'same-origin',
+});
